4473f34d05fa196315f4634b8d66cf5e0b819ea8
unknown <Administrator@WIN-HQEVFR0T2K8.hocinetest.ch>
2026-03-10 17:00:07 +0100

    latest state

---
+++ streamlit.sh
@@ -0,0 +1,5 @@
+pip install streamlit
+pip install azure-ai-projects==1.0.0
+pip install azure-ai-agents==1.0.0
+pip install azure-identity
+python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
